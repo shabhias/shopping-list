6bbaccbf930ffaca0ce04eb698a1be5102c113c4
shabhias <shabhialiyya3101@gmail.com>
2023-09-06 13:44:38 +0700

    tutorial 1 selesai

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'name': 'shabhi',
+        'class': 'PBP D'
+    }
+
+    return render(request, "main.html", context)
